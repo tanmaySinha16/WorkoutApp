eaf51cd94c20cde5cf73f58eca5f4457946e693e
tanmaySinha16 <tanmaysinha1610@gmail.om>
2023-01-20 09:00:32 +0530

    RunningApp

--- app/src/main/java/com/example/runningapp/db/RunDao.kt
+++ app/src/main/java/com/example/runningapp/db/RunDao.kt
@@ -8,10 +8,10 @@ import androidx.room.*
 interface RunDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-     suspend fun insertRun(run:Run)
+    suspend fun insertRun(run:Run)
 
     @Delete
-     suspend fun deleteRun(run:Run)
+    suspend fun deleteRun(run:Run)
 
     @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
     fun getAllRunsSortedByDate():LiveData<List<Run>>
